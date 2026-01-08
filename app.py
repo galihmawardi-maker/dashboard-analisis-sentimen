1b8b71d2cc7c371de321cada8b1602cd84f35e03
galihmawardi-maker <galih.mawardi@student.iti.ac.id>
2026-01-09 06:47:28 +0700

    Enhance text alignment styles in app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import numpy as np
 # Konfigurasi halaman
 st.set_page_config(
     page_title="Dashboard Analisis Sentimen",
-    page_icon="📊",
+    page_icon=📈",
     layout="wide",
     initial_sidebar_state="expanded"
 )
